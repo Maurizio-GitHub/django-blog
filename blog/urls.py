d5c987bbb73b6216240c26f906405828c07caeb5
Maurizio <maurizio.loffredo@outlook.com>
2023-02-21 22:43:16 +0000

    Feat: added HTML templates

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+# This is similar to what you have done before. However, because we are
+# using class-based views, we need to add the 'as_view' method at the
+# end of 'PostList'. So, it is going to render this class as a view:
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+]
